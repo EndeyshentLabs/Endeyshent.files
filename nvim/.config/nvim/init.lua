624cfde018d85171a450243864e367072b3acb8f
EndeyshentLabs <Themikfound@gmail.com>
2023-11-06 15:22:19 +0300

    [nvim] Move options from `init.lua` to `config/options.lua`

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,18 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
-vim.opt.shiftwidth = 4
-vim.opt.tabstop = 4
-vim.opt.title = true
-vim.opt.colorcolumn = "120"
-vim.opt.exrc = true -- unsafe
-vim.g.defaultguifontsize = 13
-vim.g.guifontsize = vim.g.defaultguifontsize
-vim.g.guifont = "VictorMono Nerd Font"
-vim.opt.guifont = vim.g.guifont .. ":h" .. vim.g.guifontsize
-vim.opt.langmap =
-  "ФИСВУАПРШОЛДЬТЩЗЙКЫЕГМЦЧНЯ;ABCDEFGHIJKLMNOPQRSTUVWXYZ,фисвуапршолдьтщзйкыегмцчня;abcdefghijklmnopqrstuvwxyz"
-
 if vim.g.neovide then
   vim.g.neovide_remember_window_size = true
   vim.g.neovide_remember_window_position = true
--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,3 +1,17 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+-- Options. Loaded before lazy.nvim
+
+local opt = vim.opt
+local g = vim.g
+
+g.autoformat = false
+opt.shiftwidth = 4
+opt.tabstop = 4
+opt.title = true
+opt.colorcolumn = "120"
+opt.exrc = true -- unsafe
+g.defaultguifontsize = 13
+g.guifontsize = g.defaultguifontsize
+g.guifont = "VictorMono Nerd Font"
+opt.guifont = g.guifont .. ":h" .. g.guifontsize
+opt.langmap =
+  "ФИСВУАПРШОЛДЬТЩЗЙКЫЕГМЦЧНЯ;ABCDEFGHIJKLMNOPQRSTUVWXYZ,фисвуапршолдьтщзйкыегмцчня;abcdefghijklmnopqrstuvwxyz"
