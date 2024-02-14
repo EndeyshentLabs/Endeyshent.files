ec220be31bebd7ac5651a724572675f16515e82f
EndeyshentLabs <Themikfound@gmail.com>
2024-02-14 14:39:28 +0300

    [nvim/colorschemes] Some additions

--- nvim/.config/nvim/lua/plugins/colorschemes.lua
+++ nvim/.config/nvim/lua/plugins/colorschemes.lua
@@ -1,20 +1,30 @@
 return {
   {
-    "RRethy/nvim-base16",
+    "lettertwo/laserwave.nvim",
     lazy = true,
     -- priority = 1000,
     -- config = function()
-    --   vim.cmd([[colorscheme base16-pop]])
-    -- end,
+    --   vim.cmd.colorscheme("laserwave")
+    -- end
+  },
+  {
+    "RRethy/nvim-base16",
+    -- lazy = true,
+    priority = 1000,
+    config = function()
+      vim.cmd([[colorscheme base16-pop]])
+    end,
   },
   {
     "rose-pine/neovim",
     name = "rose-pine",
-    config = function()
-      require("rose-pine").setup({
-        variant = "main",
-      })
-      vim.cmd[[colorscheme rose-pine]]
-    end
+    lazy = true,
+    -- priority = 1000,
+    -- config = function()
+    --   require("rose-pine").setup({
+    --     variant = "main",
+    --   })
+    --   vim.cmd[[colorscheme rose-pine]]
+    -- end
   }
 }
